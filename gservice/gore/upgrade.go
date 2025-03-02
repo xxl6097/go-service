8be8a9232e31a8e585bf1f2cc4b27f8f4d732d9c
xiaoli.xia <xiaoli.xia@clife.cn>
2025-03-02 21:20:40 +0800

    0.2.71 2025-03-02 21:20:40 by

--- gservice/gore/upgrade.go
+++ gservice/gore/upgrade.go
@@ -3,7 +3,6 @@ package gore
 type GlobService interface {
 	IService
 	OnUpgrade(string, string) []string
-	OnInstall(string, string) []string
 }
 
 func Upgrade(svr IService, oldBinPath string, newFileUrlOrLocalPath string) []string {
@@ -14,16 +13,5 @@ func Upgrade(svr IService, oldBinPath string, newFileUrlOrLocalPath string) []st
 }
 
 func manualUpgrade(oldBinPath string, newFileUrlOrLocalPath string) []string {
-	return nil
-}
-
-func Installs(svr IService, binPath, installBinPath string) []string {
-	if gs, ok := svr.(GlobService); ok {
-		return gs.OnInstall(binPath, installBinPath)
-	}
-	return manualInstall(binPath, installBinPath)
-}
-
-func manualInstall(binPath, installBinPath string) []string {
 	return nil
 }
