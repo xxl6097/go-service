fd2751f3c5abd5080c1a6d8e4f0c6d79b050035c
M4MacBookAir <M4MacBookAir@clife.cn>
2025-05-26 16:41:19 +0800

    v0.5.12 by uuxia

--- cmd/app/patch/main.go
+++ cmd/app/patch/main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	// 生成补丁示例（需配合bsdiff等工具）
 	patch := new(bytes.Buffer)
-	oldBytes, _ := os.Open("/Users/uuxia/Downloads/a/aatest_v0.5.9_linux_amd64")
-	newBytes, _ := os.Open("/Users/uuxia/Desktop/work/code/github/golang/go-service/release/aatest_v0.5.10_linux_amd64")
+	oldBytes, _ := os.Open("/Users/uuxia/Downloads/a/aatest_v0.5.10_linux_amd64")
+	newBytes, _ := os.Open("/Users/uuxia/Desktop/work/code/github/golang/go-service/release/aatest_v0.5.11_linux_amd64")
 
 	err := binarydist.Diff(bufio.NewReader(oldBytes), bufio.NewReader(newBytes), patch)
 	if err != nil {
 		return
 	}
-	os.WriteFile("./update.patch", patch.Bytes(), 0644)
+	os.WriteFile("./release/update.patch", patch.Bytes(), 0644)
 
 }
