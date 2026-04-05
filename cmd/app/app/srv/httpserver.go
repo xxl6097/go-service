51bffb4ea782e60ecf6742a71480ae373c2ed6df
xxl6097 <263996097@qq.com>
2026-04-05 17:37:29 +0800

    v0.7.24 by uuxia

--- cmd/app/app/srv/httpserver.go
+++ cmd/app/app/srv/httpserver.go
@@ -36,15 +36,18 @@ func init() {
 	//glog.Hook(func(bytes []byte) {
 	//	logQueue.AddMessage(string(bytes[2:]))
 	//})
-	z.Hook = func(entry zapcore.Entry) error {
-		time := entry.Time.Format(time.DateTime)
-		msg := entry.Message
-		lineNum := entry.Caller.Line
-		filepath.Base(entry.Caller.File)
-		m := fmt.Sprintf("%s %s:%d %s", time, filepath.Base(entry.Caller.File), lineNum, msg)
-		logQueue.AddMessage(m)
-		return nil
-	}
+
+	z.LoadLogger(func(conf *z.LogConfig) {
+		conf.Hook = func(entry zapcore.Entry) error {
+			time := entry.Time.Format(time.DateTime)
+			msg := entry.Message
+			lineNum := entry.Caller.Line
+			filepath.Base(entry.Caller.File)
+			m := fmt.Sprintf("%s %s:%d %s", time, filepath.Base(entry.Caller.File), lineNum, msg)
+			logQueue.AddMessage(m)
+			return nil
+		}
+	})
 }
 
 type Message[T any] struct {
